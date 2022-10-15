0611e325a160ecae76d095274034b0d355c98d23
Ученик4 <user4@school.ru>
2022-10-15 16:05:35 +1200

    мой коммит

---
+++ script.js
@@ -0,0 +1,150 @@
+//console.log('13');
+        
+//const goo = 2
+
+
+//let age = +prompt('сколько тебе лет')
+//console .log(goo+age)
+
+// let name = prompt('как тебя зовут?')
+//let str = "привет"
+
+//console.log(str + name)
+
+//let roo=true
+
+//let yoo = 'hello'== 'hello'
+
+//console.log(yoo)
+//console.log( typeof roo);
+//console.log(typeof str)
+//console.log(typeof goo)
+
+// let dr = 2 
+
+// console.log(typeof dr)
+
+// let ht = "цез"
+// console.log(typeof ht) 
+
+// let gt = true 
+// console.log(typeof gt)
+
+// let str = "fake"
+
+// console.log(str[2]);
+
+// let a = "яблоко!"
+// let a1 = "верно!"
+// let a2 = "уголь!"
+// let a3 = "мимо!"
+
+// console.log(a[1] + a1[1] + a2[1] + a3[1])
+
+// console.log(a.length);
+
+// console.log(a.indexOf('к'));
+
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase());
+// a = a.replace('яблоко', 'pizza')
+// console.log(a);
+
+// console.log(a.slice(0,4));
+
+// console.log(a1.slice(1,2).toUpperCase()+a2.slice(0,1).toLowerCase()+a3[0].toUpperCase());
+
+
+// let b = 'парламент'
+// let b1='о'
+// console.log(b.slice(0,1)+b1[0]+b.slice(2,4)+b1[0]+b.slice(5,9));
+
+// let a = 'парламент'
+// console.log(a.split('').reverse().join(''));
+
+// let age = +prompt('сколько вам лет')
+
+// if ( age <18 ){
+//     alert('вам сюда нельзя');
+// } else if (age>18){
+//     alert('вам можно сюда')
+// } else {
+//     console.log('респект пабло')
+// }
+
+// let a = confirm('будете играть?')
+// if(a){
+// alert('вы приходите к болльшой пещере и слышите внутри какие-то звуки...')
+// let a2 = confirm('Вы пойдете в пещеру?') 
+// if (a2) {
+//     alert('вы вошли в пещеру и тут очень темно')
+//     let a3 = confirm('зажечь факел?')
+//     if (a3) {
+//         alert('вы пошли в глубь')
+//         let a4 = confirm('на вас напал слизь...Вступите в битву?')
+//             if (a4) {
+//                 alert('вы достали меч... слизь оказалась королем слизней и вы погибли')
+//             } else {
+//                 alert('вы убежали')
+//             }
+//             let a5 = confirm('Перед вами темный проход с факелами...ВЫ пойдете туда?')
+// if (a5) {
+//     alert ('вы попали в илюзию гипно-грибов')
+// } else {
+//     alert ('вы пошли назад')
+// }  
+//     } else {
+//         alert('вы упали и разбились насмерть')
+//     } 
+// } else {
+//     alert('вы убежали поджав хвост') 
+// }
+// } else {
+//     alert ('ну и дите по добру по здорову')
+// }
+
+// let nn = false 
+// let nn1 = true
+// let nn2 = 1
+// let nn3 = null
+// nn ? console.log('false') : nn1 ? console.log('true') : nn2 ? console.log('1') : nn3===false ? console.log('true') : console.log('false');
+
+// let b = +prompt('вы будете играть?... -да,b-нет')
+// b ? console.log('a') : console.log('');
+
+
+// a ? alert ('вы увидели пещеру') : a ? null : alert('ладно')
+// let a1 = confirm('вы войдете в пещеру?')
+// a1 ? alert ('вы вошли в пещеру') : a1 ? null: alert ('ну и идите по добру по здорову')
+// let a2 = confirm('зажечь факел?')
+// a2 ? alert ('вы пошли в глубь') : a2 ? null: alert ('вы пошли и свалились в яму')
+// let a3 = confirm('вы наткнулись на слизня...Начать битву?')
+// a3 ? alert ('Это оказался король слизней... Вы погибли'): a3 ? null: alert ('вы убежали и спаслись'): console.log)
+
+// let a = +prompt('дайте число от 1 до 6')
+
+// switch(a) {
+// case 1: 
+// console.log('1')
+// break 
+// case 2:
+//     console.log('2');
+//     break
+//     case 3:
+//         console.log('3')
+//         break
+//         case 4:
+//             console.log('4')
+//             break 
+//             case 5:
+//             console.log('5')
+//             break 
+//             case 6:
+//             console.log('6')
+//             break
+// default:
+//     console.log('ничего');
+//     break
+// }
+
+
